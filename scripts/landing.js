fd28f44e59f3731c25235aa66a4977be859f71f1
Samantha <sam.teldin@gmail.com>
2018-07-15 21:43:15 -0700

    Assignment 23 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,25 @@
-var animatePoints = function() {
-           var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-           var revealPoint = function(index) {
-             points[index].style.opacity = 1;
-             points[index].style.transform = "scaleX(1) translateY(0)";
-             points[index].style.msTransform = "scaleX(1) translateY(0)";
-             points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-           }
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
-           for (var i = 0; i < points.length; i++) {
-             revealPoint(i);
-           }
-       };
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
+};
+
+window.onload = function() {
+
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+
+    window.addEventListener("scroll", function(event) {
+        if (document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
