f44c46076f876540b270f32fbf7e61b95f21961d
Samantha <sam.teldin@gmail.com>
2018-07-18 11:39:28 -0700

    Checkpoint 32 Completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21' },
+         { title: 'Magenta', duration: '2:15' }
+     ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+ // Third Example Album
+ var albumPrince = {
+   title: 'Purple Rain',
+   artist: 'Prince',
+   label: 'EM',
+   year: '1999',
+   albumArtUrl: 'assets/images/album_covers/20.png',
+   songs: [
+     { title: 'When Doves Cry', duration: '1:41' },
+     { title: 'Little Red Corvette', duration: '3:01' },
+     { title: '1999', duration: '3:21'},
+     { title: 'Little Red Corvette', duration: '2:14' },
+     { title: 'Raspberry Beret', duration: '3:15'}
+   ]
+ };
