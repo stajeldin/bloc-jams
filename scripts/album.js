a1fd396ad5b6faa5a13b17606987bfe1addf52a5
Samantha <sam.teldin@gmail.com>
2018-08-02 22:23:24 -0700

    Working on fixing assignment 34

--- scripts/album.js
+++ scripts/album.js
@@ -3,6 +3,7 @@ var getSongNumberCell = function(number) {
 };
 
 var createSongRow = function(songNumber, songName, songLength) {
+    var template =
         '<tr class="album-view-song-item">'
       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
