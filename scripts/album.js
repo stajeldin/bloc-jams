b1bd88dfa6334e62f04d8077cd396345ecbc9d04
Samantha <sam.teldin@gmail.com>
2018-08-10 10:30:39 -0700

    Assignment 34 done

--- scripts/album.js
+++ scripts/album.js
@@ -29,17 +29,6 @@ var createSongRow = function(songNumber, songName, songLength) {
         updatePlayerBarSong();
 
 
-      } else if (currentlyPlayingSongNumber === songNumber) {
-          if (currentSoundFile.isPaused()) {
-              $(this).html(pauseButtonTemplate);
-              $('.main-controls .play-pause').html(playerBarPauseButton);
-              currentSoundFile.play();
-              updateSeekBarWhileSongPlays();
-          } else {
-              $(this).html(playButtonTemplate);
-              $('.main-controls .play-pause').html(playerBarPlayButton);
-              currentSoundFile.pause();
-          }
       }
   };
 
